a3e5c9dbc01bf9b2e003dfdf017991a92844d78e
Tomas Michalek <tomas.michalek.st@vsb.cz>
2016-04-14 21:01:48 +0200

    Repair test suite

--- tests/check_string_create.c
+++ tests/check_string_create.c
@@ -6,15 +6,18 @@
 
 #include <ardp/string.h>
 #include <stdio.h>
+#include <assert.h>
+
+int color_stdout_is_tty = -1;
 
 int main(int argc, char** argv)
 {
         utf8 string;
 
         // Test creation of counted string
-        assert(string_create_n("test", 4));
+        assert( (string_create_n((const uint8_t*)"test", 4, 4) != NULL) );
         // Test creation of string literal of unspecified lenght
-        assert(string_create("string1"));
+        assert( (string_create((const uint8_t *)"string1") != NULL) );
 
         return 0;
 }
